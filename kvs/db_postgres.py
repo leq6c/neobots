5b8ccd6da1ea6dfb93c4b53e0b49b468bf255d1b
leq6c <leq6c6571@outlook.jp>
2025-03-22 21:46:00 +0900

    kvs: use connection pool

--- kvs/db_postgres.py
+++ kvs/db_postgres.py
@@ -1,32 +1,56 @@
 import os
+
 import psycopg2
 from psycopg2 import sql
+from psycopg2.pool import ThreadedConnectionPool
 
 DB_CONNECTION = os.getenv("DB_CONNECTION")
+TABLE_NAME = "data_store_0"
+
 
 class DBPostgres:
     def __init__(self):
         self.connection = None
 
     def init_db(self):
-        self.connection = psycopg2.connect(DB_CONNECTION)
-        with self.connection.cursor() as cursor:
-            cursor.execute(
-                sql.SQL(
-                    "create table if not exists data_store (data_key varchar(255) primary key, data_value text);"
+        self.pool = ThreadedConnectionPool(
+            1, 10, dsn=DB_CONNECTION  # minconn  # maxconn
+        )
+
+        conn = self.pool.getconn()
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    sql.SQL(
+                        f"create table if not exists {TABLE_NAME} (data_key varchar(255) primary key, data_value text);"
+                    )
                 )
-            )
+            conn.commit()
+        finally:
+            self.pool.putconn(conn)
 
     def put(self, key, value):
-        with self.connection.cursor() as cursor:
-            cursor.execute(
-                "INSERT INTO data_store (data_key, data_value) VALUES (%s, %s)", (key, value)
-            )
+        conn = self.pool.getconn()
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    f"INSERT INTO {TABLE_NAME} (data_key, data_value) VALUES (%s, %s)",
+                    (key, value),
+                )
+            conn.commit()
+        finally:
+            self.pool.putconn(conn)
 
     def get(self, key):
-        with self.connection.cursor() as cursor:
-            cursor.execute("SELECT data_value FROM data_store WHERE data_key = %s", (key,))
-            row = cursor.fetchone()
-            if row is None:
-                return None
-            return row[0]
+        conn = self.pool.getconn()
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    f"SELECT data_value FROM {TABLE_NAME} WHERE data_key = %s", (key,)
+                )
+                row = cursor.fetchone()
+                if row is None:
+                    return None
+                return row[0]
+        finally:
+            self.pool.putconn(conn)
